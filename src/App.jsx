728cb8031eb5fc35d8d0b003cd4bc11ada46fbf8
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-15 19:07:54 +0530

    conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,41 @@
 import { useState, useEffect } from "react"
-
-function App (){
+// conditonal rendering 
+function App() {
+  let counterVisible = true;
   return <div>
-  <Counter></Counter>
+   {counterVisible ?  <Counter></Counter> : null }
+   {counterVisible && <Counter></Counter>}
   </div>
 }
 
 // mounting , re rendering , unmounting 
-function Counter(){
- const [count , setCount] = useState(0);
-// hooking into lifecycle events of react
- console.log("counter")
+function Counter() {
+  const [count, setCount] = useState(0);
+  // hooking into lifecycle events of react
+  console.log("counter")
 
-//  setInterval (function(){
-//   setCount ( count + 1);
-//  }, 1000)
+  //  setInterval (function(){
+  //   setCount ( count + 1);
+  //  }, 1000)
 
-// guard our set intervals from re renders 
-useEffect (function(){
-  setInterval (function(){
-    setCount (count => count + 1 );
-  }, 1000)
-  console.log ("mounted");
-}, []);
+  // guard our set intervals from re renders 
+  useEffect(function () {
+    setInterval(function () {
+      setCount(count => count + 1);
+    }, 1000)
+    console.log("mounted");
+  }, []);
 
+  function increaseCount() {
+    setCount(count + 1);
+  }
 
   return <div>
     <h1 id="text">{count}</h1>
-    
+    <button onClick={increaseCount}>increasecount</button>
   </div>
- 
+
 }
 
 
- export default App 
+export default App 
