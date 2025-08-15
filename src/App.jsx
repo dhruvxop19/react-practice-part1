b61e7012d3ce8b65f1e1c24d364ae21a644b0b5e
dhruvxop19 <dhruvxop19@gmail.com>
2025-08-15 15:23:52 +0530

    changed to set interval

--- src/App.jsx
+++ src/App.jsx
@@ -14,25 +14,14 @@ function App (){
 function Counter(){
  const [count , setCount] = useState(0);
 
-  function increaseCount (){
-setCount (count + 1);
-  }
-
-function decreaseCount (){
-  setCount(count - 1);
-}
-
-function resetCount (){
-  setCount (0);
-}
+ setInterval (function(){
+  setCount ( count + 1);
+ }, 1000)
 
   return <div>
     <h1 id="text">{count}</h1>
-  <button onClick={increaseCount}>Increase Count
-  </button>
-  <button onClick={decreaseCount}>Decrease Count</button>
-  <button onClick={resetCount}>reset</button>
   </div>
+ 
 }
 
 
